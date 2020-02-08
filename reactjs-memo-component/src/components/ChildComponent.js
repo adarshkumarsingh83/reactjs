7d4260d94c07655425840308bce2528affeb5593
Kumar <akumar38@contractor.jcp.com>
2020-02-07 23:05:19 -0600

    adding

--- reactjs-memo-component/src/components/ChildComponent.js
+++ reactjs-memo-component/src/components/ChildComponent.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-function ChildComponent({ name }) {
+const ChildComponent = ({ name }) => {
   console.log("ChildComponent render()");
   return <h1>{name}</h1>;
-}
+};
 
 export default ChildComponent;
