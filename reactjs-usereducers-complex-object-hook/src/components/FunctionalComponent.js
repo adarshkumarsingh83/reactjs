3e2cf13b65f7fbee5d11924d2b72c0f7935d9271
Kumar <akumar38@contractor.jcp.com>
2020-02-16 00:07:00 -0600

    adding use reducer with complex object hook

--- reactjs-usereducers-complex-object-hook/src/components/FunctionalComponent.js
+++ reactjs-usereducers-complex-object-hook/src/components/FunctionalComponent.js
@@ -1,18 +1,25 @@
 import React from "react";
 
 const dataStoreInitValue = {
-  countValue: 0
+  countValue1: 0,
+  countValue2: 0
 };
 
 const reducerFunction = (state, action) => {
-  console.log(`${state.countValue} ${action.type} ${action.value} `);
+  console.log(`${state.countValue1} ${action.type} ${action.value} `);
   switch (action.type) {
-    case "increment":
-      return { countValue: state.countValue + action.value };
-    case "decrement":
-      return { countValue: state.countValue - action.value };
-    case "reset":
-      return dataStoreInitValue;
+    case "increment1":
+      return { ...state, countValue1: state.countValue1 + action.value };
+    case "decrement1":
+      return { ...state, countValue1: state.countValue1 - action.value };
+    case "reset1":
+      return { ...state, countValue1: 0 };
+    case "increment2":
+      return { ...state, countValue2: state.countValue2 + action.value };
+    case "decrement2":
+      return { ...state, countValue2: state.countValue2 - action.value };
+    case "reset2":
+      return { ...state, countValue2: 0 };
     default:
       return state;
   }
@@ -28,18 +35,41 @@ const FunctionalComponent = () => {
     <div>
       <br />
       <br />
-      <h1>Value of coutner {dataStore.countValue} </h1>
+      <h1>Value of coutner {dataStore.countValue1} </h1>
       <br />
-      <button onClick={() => dispatcherAction({ type: "increment", value: 2 })}>
-        INCREMENT
+      <button
+        onClick={() => dispatcherAction({ type: "increment1", value: 2 })}
+      >
+        INCREMENT COUNTER 1
       </button>
       &nbsp;
-      <button onClick={() => dispatcherAction({ type: "decrement", value: 2 })}>
-        DECREMENT
+      <button
+        onClick={() => dispatcherAction({ type: "decrement1", value: 2 })}
+      >
+        DECREMENT COUNTER 1
       </button>
       &nbsp;
-      <button onClick={() => dispatcherAction({ type: "reset", value: 0 })}>
-        RESET
+      <button onClick={() => dispatcherAction({ type: "reset1", value: 0 })}>
+        RESET COUNTER 1
+      </button>
+      <br />
+      <br />
+      <h1>Value of coutner {dataStore.countValue2} </h1>
+      <br />
+      <button
+        onClick={() => dispatcherAction({ type: "increment2", value: 2 })}
+      >
+        INCREMENT COUNTER 2
+      </button>
+      &nbsp;
+      <button
+        onClick={() => dispatcherAction({ type: "decrement2", value: 2 })}
+      >
+        DECREMENT COUNTER 2
+      </button>
+      &nbsp;
+      <button onClick={() => dispatcherAction({ type: "reset2", value: 0 })}>
+        RESET COUNTER 2
       </button>
     </div>
   );
