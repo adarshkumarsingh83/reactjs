c704ddd18f525d9aae5629af81abb87a05eb0811
Kumar <akumar38@contractor.jcp.com>
2020-02-17 18:26:50 -0600

    spring boot and reactjs usereducer data fetch

---
+++ reactjs-usereducer-data-fetch/reactjs-data-fetching/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./css/App.css";
+import DataRepository from "./components/DataRepository";
+
+function App() {
+  return (
+    <div className="App">
+      <DataRepository />
+    </div>
+  );
+}
+
+export default App;
---
+++ reactjs-usereducer-data-fetch/reactjs-data-fetching/src/components/DataRepository.js
@@ -0,0 +1,44 @@
+import React from "react";
+import axios from "axios";
+
+const DataRepository = () => {
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState("");
+  const [employees, setEmployees] = React.useState([]);
+
+  React.useEffect(() => {
+    axios
+      .get("http://localhost:8080/employees")
+      .then(response => {
+        setLoading(false);
+        setError("");
+        setEmployees(response.data);
+      })
+      .catch(error => {
+        setLoading(false);
+        setError("Error while Data Loading");
+        setEmployees([]);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1> {error ? error : null} </h1>
+      <ul>
+        {loading ? "loading..." : null}
+        {employees.map(employee => {
+          return (
+            <ol key={employee.id}>
+              {employee.id}&nbsp;
+              {employee.firstName}&nbsp;
+              {employee.lastName}&nbsp; 
+              {employee.email}
+            </ol>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default DataRepository;
