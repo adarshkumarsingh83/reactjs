d366b4da8a5c248e927ebf6ee35cb679243716ef
Kumar <akumar38@vv46813.jcp.com>
2020-02-28 11:49:27 -0600

    adding reactjs pie chart

---
+++ reactjs-piechar/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/App.css';
+import PieChartComponent from './components/PieChartComponent'
+function App() {
+  return (
+    <div className="App">
+    <PieChartComponent/>
+    </div>
+  );
+}
+
+export default App;
