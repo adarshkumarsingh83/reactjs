aa73bda5fdd02c6f2fe50d65682fdfc9686b198f
adarsh <us-guest@USMB113823-2.local>
2020-09-10 19:57:47 -0500

    updating

--- APPLICATIONS/spring-boot-reactjs/src/main/java/com/espark/adarsh/dao/Employee.java
+++ APPLICATIONS/spring-boot-reactjs/src/main/java/com/espark/adarsh/dao/Employee.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2015 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.espark.adarsh.dao;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -22,10 +7,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Version;
 
-/**
- * @author Greg Turnquist
- */
-// tag::code[]
 @Entity
 public class Employee {
 
@@ -113,4 +94,3 @@ public class Employee {
 			'}';
 	}
 }
-// end::code[]
